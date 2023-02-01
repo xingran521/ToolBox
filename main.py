19cdecd5b55564cdc541368a379d3295ee729018
xingran521 <xingran01@qq.com>
2023-02-01 10:02:29 +0800

    main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# 主调用程序
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    : main.py
+@version :1.0
+@Time    : 2023/02/01 09:59:35
+@Author  : cxr
+@Desc    : None
+'''
+
+# 导入库
+
+
+class ToolBox:
+    
+    def __init__():
+        pass
+    def cs():
+        pass
+    def main():
+        pass
+
+if "__name__" in __init__:
+    ToolBox()
